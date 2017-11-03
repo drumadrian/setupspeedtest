7315f6f47b857a3d5d0e5da530bf8873a38c1a6b
drumadrian <adrian.drummond@gmail.com>
2017-11-03 07:23:22 -0700

    udpated scripts and names

--- how_to_execute_setup_speedtest_server.bash
+++
@@ -1,9 +0,0 @@
-#Install tools
-sudo yum install -y telnet
-sudo yum install -y git
-
-#execute_setup_speedtest_server
-cd /home/ec2-user
-git clone https://github.com/drumadrian/setupspeedtest.git
-cd setupspeedtest
-sudo -u ec2-user bash setup_speedtest_server.bash
