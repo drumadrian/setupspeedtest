d516f26637658e59c428b5c0fba61915f2eba4b1
drumadrian <adrian.drummond@gmail.com>
2017-11-03 19:22:41 -0700

    updates for my file for downloading

--- setup_speedtest_server.bash
+++ setup_speedtest_server.bash
@@ -6,7 +6,7 @@
 # https://stackoverflow.com/questions/7642674/how-do-i-script-a-yes-response-for-installing-programs
 # https://github.com/soterinsights/speedtest
 # https://github.com/drumadrian/setupspeedtest
-
+# https://www.skorks.com/2010/03/how-to-quickly-generate-a-large-file-on-the-command-line-with-linux/
 
 #Install tools
 sudo yum install -y telnet
@@ -41,6 +41,47 @@ echo "###################### START: ADDED BY setup_speedtest_server.bash #######
 echo "sudo -u ec2-user bash /home/ec2-user/setupspeedtest/runspeedtestapp.bash" | sudo tee --append /etc/rc.local
 echo "" | sudo tee --append /etc/rc.local
 echo "###################### END: ADDED BY setup_speedtest_server.bash ######################" | sudo tee --append /etc/rc.local
+
+
+
+
+
+
+
+# setup adrian files for download testing
+sudo yum update -y
+sudo yum update -y
+sudo yum install -y httpd24
+sudo service httpd start
+sudo chkconfig httpd on
+chkconfig --list httpd
+sudo usermod -a -G apache ec2-user
+ls -al /var/www/
+sudo chown -R ec2-user:apache /var/www
+ls -al /var/www/
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+
+
+
+#create 1GB file
+cd /var/www/html
+dd if=/dev/zero of=file.txt count=1048576 bs=1024
+touch healthcheck.html
+touch index.html
+echo "Hello SpeedTester" | sudo tee --append index.html
+echo "Healthy" | sudo tee --append healthcheck.html
+
+
+#Re-Setup all permissions
+ls -al /var/www/
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+ls -al /var/www/
+
 
 
 
