2c80a4159943182715e511380624a13da167ca71
drumadrian <adrian.drummond@gmail.com>
2017-11-21 00:09:02 -0800

    updated script and python

--- client_code/lambda_function.py
+++ client_code/lambda_function.py
@@ -6,12 +6,13 @@ from boto3.dynamodb.conditions import Key, Attr
 import os
 dynamodb = boto3.resource('dynamodb')
 s3 = boto3.resource('s3')
-# import json
+import json
+from json2html import *
 
 # https://stackoverflow.com/questions/4706499/how-do-you-append-to-a-file
 # https://stackoverflow.com/questions/36780856/complete-scan-of-dynamodb-with-boto3
 # https://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Client.upload_file
-
+# https://stackoverflow.com/questions/14150854/aws-s3-display-file-inline-instead-of-force-download
 
 print('Loading function')
 
@@ -31,18 +32,39 @@ def lambda_handler(event, context):
     response = table.scan()
     data = response['Items']
 
+    #print html to console for testing
+    html = json2html.convert(json = data)
+    print (html)
+
     with open("/tmp/index.html", "w") as myfile:
         # index_file_contents = "<!DOCTYPE html> <html><body>" + json.dumps(data) + "</body>" 
-        index_file_contents = json.dumps(data)
+        # index_file_contents = json.dumps(data)
+        index_file_contents = html
         myfile.write(index_file_contents)
         myfile.close()
 
 
-    s3.meta.client.upload_file('/tmp/index.html', 'data.speedtest.adrianws.com', 'index.html')    
-    s3.meta.client.upload_file('/tmp/index.html', 'data.speedtest.adrianws.com', 'data.html')    
+    s3.meta.client.upload_file('/tmp/index.html', 'data.speedtest.adrianws.com', 'index.html', ExtraArgs={'ContentType': "text/html"})    
+    s3.meta.client.upload_file('/tmp/index.html', 'data.speedtest.adrianws.com', 'data.html', ExtraArgs={'ContentType': "text/html"})    
 
     return "Good"
 
 
 
 # https://stackoverflow.com/questions/31011179/converting-json-to-html-table-in-python
+
+
+
+if __name__ == "__main__":
+    my_event = {"Records":[
+
+    {
+      "eventID": "1",
+      "eventName": "1.0",
+      "dynamodb": "1.0",
+    }
+
+    ]}
+    lambda_handler(my_event, "blah2") 
+
+
